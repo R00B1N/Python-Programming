f36f2fab918d5b3f19e1fdcaaf7f340fad3c0ff1
Blacksterhack <blastertv1001@gmail.com>
2021-08-07 14:09:46 -0500

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+answer = input("Do you want to hear a joke? ")
+
+if answer == "Yes":
+    print("I'm against picketing, but I don't know how to show it.")
+    # Mitch Hedberg (RIP)
+elif answer == "No":
+    print("Fine.")
+else:
+    print("I don't understand.")
