fc80e9f2693118dcd76050fb501f849f47759182
Blacksterhack <blastertv1001@gmail.com>
2021-08-07 14:33:05 -0500

    in

---
+++ in.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+answer = input("Do you want to hear a joke? ")
+
+affirmative_responses = ["yes", "y"]
+negative_responses = ["no", "n"]
+
+if answer.lower() in affirmative_responses:
+    print("I'm against picketing, but I don't know how to show it.")
+    # Mitch Hedburg (RIP)
+elif answer.lower() in negative_responses:
+    print("Fine.")
+else:
+    print("I don't understand.")
