5b960ecf6cd1553041541c92fd0b52374c2b1fc6
Blacksterhack <blastertv1001@gmail.com>
2021-08-07 13:41:11 -0500

    working_with_strings

---
+++ strings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+kanye_quote = """My greatest pain in life
+is that I will never be able
+to see myself perform live."""
+
+print(kanye_quote)
+
+hamilton_quote = 'Well, the word got around, they said, "This kid\'s insane, man"'
+
+print(hamilton_quote)
+
+name = "Mattan Griffel"
+orphan_fee = 200
+teddy_bear_fee = 121.8
+
+total = orphan_fee + teddy_bear_fee
+
+# print(name, "the total will be", total)
+print(f"{name} the total will be ${total:.2f}")
